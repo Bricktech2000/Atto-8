f0cdd2ac0686550861704a883f5b4266d749a2f4
Emilien Breton <bricktech2000@gmail.com>
2023-05-12 23:54:47 -0400

    updated encoder

--- enc/enc.py
+++ enc/enc.py
@@ -1,25 +1,21 @@
 import sys
-import os
 
 # https://stackoverflow.com/questions/5649407/how-to-convert-hexadecimal-string-to-bytes-in-python
 # https://stackoverflow.com/questions/24066904/most-pythonic-way-to-extend-a-list-to-exactly-a-certain-length
 
 
 def pad_or_slice(L, n):
-  if len(L) < n:
-    return L + ([0] * (n - len(L)))
-  else:
-    return L[:n]
+  return L + ([0] * (n - len(L))) if len(L) < n else L[:n]
 
 
 if len(sys.argv) != 3:
-  print("Usage: enc.py <input file> <output file>")
+  print("Usage: enc.py <hex file> <image file>")
   sys.exit(1)
 
-input = sys.argv[1]
-output = sys.argv[2]
+hex_file = sys.argv[1]
+image_file = sys.argv[2]
 
-with open(input, 'rb') as input_file:
-  with open(os.path.splitext(output)[0] + '.bin', 'wb') as output_file:
+with open(hex_file, 'rb') as input_file:
+  with open(image_file, 'wb') as output_file:
     output_file.write(bytes(pad_or_slice(list(bytes.fromhex(
-        ''.join(list(filter(lambda line: not line.startswith('#'), input_file.read().decode('utf-8').split('\n')))))), 0x100)))
+        ''.join(list(filter(lambda line: not line.startswith('# ') and line != '#', input_file.read().decode('utf-8').split('\n')))))), 0x100)))
