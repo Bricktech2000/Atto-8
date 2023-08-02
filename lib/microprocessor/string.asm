fa8f5546b2db61d0535e643807b963737e966b77
Emilien Breton <bricktech2000@gmail.com>
2023-08-02 13:35:10 -0400

    implemented `strcpy` and `memcmp` in `string.asm`

--- lib/microprocessor/string.asm
+++ lib/microprocessor/string.asm
@@ -1,18 +1,5 @@
 strcat.def! strcat: @err # to be implemented
 strchr.def! strchr: @err # to be implemented
-strcmp.def!
-  strcmp: clc # cpm = strcmp(*str1, *str2)
-    ld2 ld2 loop.
-      # break if *str1 == '\0'
-      ld1 lda !char.null xor .break !bcs
-      # break if *str1 != *str2
-      ld1 lda xor .break !bcc pop
-    swp inc swp inc .loop !jmp break.
-    # compute and store *str1 - *str2
-    pop lda swp lda clc sub
-  # return*
-  st2 !rt1
-strcpy.def! strcpy: @err # to be implemented
 strlen.def!
   strlen: # len = strlen(*str)
     ld1 for_c.
@@ -20,21 +7,50 @@ strlen.def!
     inc .for_c !bcc
     # compute and store length
     ld2 sub
-  # return*
+  # return* len
   st1 !rt0
+strcpy.def!
+  strcpy: # strcpy(*dst, *src)
+    ld2 ld2 for_c.
+      # loop if *dst != '\0'
+      ld1 lda !char.null xor
+      ld1 sta
+    swp inc swp inc .for_c !bcc pop pop
+  # return*
+  !rt2
+strcmp.def!
+  strcmp: # cpm = strcmp(*str1, *str2)
+    ld2 ld2 for_c.
+      # break if *str1 != *str2
+      ld1 lda ld1 lda sub buf st4 .break !bcc
+      # loop if *str1 != '\0'
+      ld0 lda !char.null xor pop
+    swp inc swp inc .for_c !bcc break. pop pop
+  # return* *str1 - *str2
+  !rt1
 
 memchr.def! memchr: @err # to be implemented
-memcmp.def! memcmp: @err # to be implemented
+memset.def!
+  memset: # memset(*ptr, val, len)
+    ld3 for_i. dec
+      ld3 ld1 ld4 add sta
+    buf .for_i !bcc pop
+  !rt3
 memcpy.def!
-  memcpy: clc # memcpy(dst, src, len)
+  memcpy: clc # memcpy(*dst, *src, len)
     ld3 for_i. dec
       ld0 ld4 add lda
       ld1 ld4 add sta
     buf .for_i !bcc pop
   !rt3
-memset.def!
-  memset: # memset(ptr, val, len)
+memcmp.def!
+  memcmp: clc # cpm = memcmp(*ptr1, *ptr2, len)
     ld3 for_i. dec
-      ld3 ld1 ld4 add sta
-    buf .for_i !bcc pop
-  !rt3
+      # break if *ptr1 != *ptr2
+      ld0 ld4 add lda
+      ld1 ld4 add lda
+      sub buf st4 .break !bcc
+      # loop if i < len
+    buf .for_i !bcc break. pop
+  # return* *ptr1 - *ptr2
+  !rt2
--- test/ore.asm
+++ test/ore.asm
@@ -3,23 +3,30 @@
 @ lib/microprocessor/string.asm
 
 main!
-  x05 :fib !call
-  x0A :fib !call
-  x0D :fib !call
-  :str_abc :strlen !call
-  :str_abcdef :strlen !call
-  :str_ac :str_ab :strcmp !call
-  :str_ab :str_abc :strcmp !call
-  :str_abc :str_abc :strcmp !call
+  x05 :fib !call # 0x05
+  x0A :fib !call # 0x37
+  x0D :fib !call # 0xE9
+  :str_abc :strlen !call # 0x03
+  :str_abcdef :strlen !call # 0x06
+  :str_ac :str_ab :strcmp !call # 0x01
+  :str_ab :str_abc :strcmp !call # 0x9D
+  :str_abc :str_abc :strcmp !call # 0x00
+  :str_abcdef :str_buf :strcpy !call #
+  :str_buf :strlen !call # 0x06
+  x06 :str_abcdef :str_buf :memcmp !call # 0x00
+  x03 :str_ac :str_ab :memcmp !call # 0x01
   !hlt
 
   str_ab: d61 d62 d00
   str_ac: d61 d63 d00
   str_abc: d61 d62 d63 d00
   str_abcdef: d61 d62 d63 d64 d65 d66 d00
+  str_buf: dCC dCC dCC dCC dCC dCC dCC dCC
 
   !strlen.def
+  !strcpy.def
   !strcmp.def
+  !memcmp.def
 
   fib: clc # u8 f = fib(u8 n)
     x00 x01 # initial values
@@ -28,4 +35,3 @@ main!
     x01 su4 :for_n !bcc
     # return*
     st2 pop !rt0
-
