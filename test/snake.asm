5482a93e72e714771d575ea58e798a8bed8f6577
Emilien Breton <bricktech2000@gmail.com>
2023-09-22 03:47:35 -0400

    fixed `snake.asm` food spawning mechanic

--- test/snake.asm
+++ test/snake.asm
@@ -12,40 +12,43 @@ main!
   xE7 !u4u4 # head_pos
   xF0 !u8 # head_vel
 
-  # seeds determined through trial and error
-  x15 # rand_seed, 14 steps
-  # x2A # rand_seed, 13 steps
-  # x54 # rand_seed, 12 steps
-  # food_pos = rand_seed | 0x11
+  xCD # rand_seed
+      # food_pos = rand_seed | 0x11
 
   x00 not @const # frame parity
 
   food:
-    ld1 !rand.min st1
+    ld1
+      # not enough memory to increment score
+      # x00 ad8 @dyn # clears carry
+      # `!rand.min`, but with hand-tuned `rand_seed` and `!rand_bits` that
+      # maximizes cycle length while ensuring food never spawns on a wall
+      shl x01 x00 iff xor
+    st1
 
   loop: not
     x12 !delay
 
-    # draw food at food_pos
-    ld1 x11 orr !display_buffer !bit_addr !set_bit
-
     # default: `head_vel`
-    !getc !u8.ld0+3 !primary_to_delta st0
+    !getc !u8.ld3 !primary_to_delta st0
     # on even frame parity, ignore user input
-    !u8.ld0+3 ld2 !is_zero !u8.iff !u8.st0+2
+    !u8.ld3 ld2 !is_zero !u8.iff !u8.st2
     # head_pos += head_vel
     clc ld2 ad4
 
     # not enough memory to avoid snake wrapping around
-    # !u4u4.ld1+2 !u4u4.fst !is_zero :game_over !bcs
-    # !u4u4.ld1+2 !u4u4.snd !is_zero :game_over !bcs
+    # !u4u4.ld3 !u4u4.fst !is_zero !here !bcs
+    # !u4u4.ld3 !u4u4.snd !is_zero !here !bcs
 
-    # # if head_pos == food_pos, spawn a new food
-    ld1 x11 orr !u4u4.ld1+3 xor pop :food !bcs
+    # draw food at `food_pos`
+    ld1 x11 orr ld0 !display_buffer !bit_addr !set_bit # bleeds `food_pos` onto the stack
+
+    # if head_pos == food_pos, spawn new food
+    !u4u4.ld4 xor pop :food !bcs # consumes `food_pos` from the stack
     # compute bit_addr of head_pos
-    !u4u4.ld1+2 !display_buffer !bit_addr
+    !u4u4.ld3 !display_buffer !bit_addr
     # game over if pixel at head_pos is set
-    !u8u8.ld0 !load_bit !is_zero :game_over !bcc
+    !u8u8.ld0 !load_bit !is_zero !here !bcc
     # set pixel at head_pos
     !set_bit
 
@@ -55,18 +58,22 @@ main!
     # figure out where the tail is headed
     !u4u4.ld4 # default: tail_pos
     !directions_len for_dir: dec
+      # compute test_pos = tail_pos + direction
       :directions ld1 add !u8.lda !u4u4.ld7 !u8.add
+      # load pixel at test_pos
       !u4u4.ld0 !display_buffer !bit_addr !load_bit
+      # store test_pos if pixel is set
       shr @dyn pop if2
     !check_zero :for_dir !bcc pop
 
     # save computed `tail_pos`
     !u4u4.st4
 
-  :loop !jmp
-
-  game_over:
-    !hlt
+    # not enough memory to check if food spawned on snake body.
+    # therefore, if food_pos == tail_pos, generate new food somewhere else.
+    # otherwise, the algorithm above confuses food for the snake body.
+    ld1 x11 orr !u4u4.ld5 xor pop
+  :loop :food iff !jmp
 
   directions:
     @F0 !i4i4
